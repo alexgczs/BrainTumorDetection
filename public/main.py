0409f03e4f2919dd69f1d86efad09fd757ba30c3
Alvaro de la Maza <alvarodlmaza@gmail.com>
2022-10-29 12:01:17 +0200

    notebook

--- public/main.py
+++ public/main.py
@@ -1,5 +1,12 @@
 from ast import arg
+import imp
 import streamlit as st
+import time
 
-st.title("Brain Tumor Detection")
-st.file_uploader("Upload your photos")
+if __name__ == '__main__':
+    st.title("Brain Tumor Detection")
+    image = st.file_uploader("Upload your photos", accept_multiple_files= False)
+    if image != None:
+        with st.spinner("processing"):
+            time.sleep(3)
+        st.image(image)
