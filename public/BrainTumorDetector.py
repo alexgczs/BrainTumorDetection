2dc02b7a5fc499ae7699bc687d8dd5c06683e03a
igaston <itogaston.2@gmail.com>
2022-10-30 15:40:08 +0100

    Fix image reading

--- public/BrainTumorDetector.py
+++ public/BrainTumorDetector.py
@@ -1,32 +1,40 @@
 from torchvision.models import vit_b_16, ViT_B_16_Weights
-import torchvision.transforms as T
+import torchvision.transforms as transforms
 from PIL import Image
+from torch import unsqueeze, max, from_numpy
+from torch.nn import Linear
+from numpy import array
 
-class BrainTumorDetector():
+class BrainTumorDetector:
     def __init__(self) -> None:
+        self.__init_model()
+
+    def __init_model(self):
         self.model = vit_b_16(weights=ViT_B_16_Weights.DEFAULT)
+        self.model.heads = Linear( self.model.heads.head.in_features, 1)
+
+    def __preprare_img(self, image):
+        input = Image.open(image).convert("RGB")
+        input = transforms.Resize((224, 224))(input)
+        tensor = transforms.ToTensor()(input)
+        tensor = unsqueeze(tensor, dim= 0)
+        return tensor
 
     def evaluate(self, images):
+        preds = from_numpy(array([[0.0]]))
         for image in images:
-            input = Image.open(image)
-            input = T.Resize((224,224))(input)
+            tensor = self.__preprare_img(image)
 
-            output = self.model(input)
-            print("out", output)
+            output = self.model(tensor)
 
-    def evaluate_v0(self, imagen):
-        input=np.array([imagen])
-        input.resize((1,3,224,224))
-        input = torch.from_numpy(input)
-        input = input.type(torch.float32)
-        output=self.model(input)
-        print("out",output)
-        _,pred=torch.max(output, 1)
-        return pred
+            print("pred", output)
+            # _,pred= max(output, 1)
 
-    def get_cancer(self):
-        return self.cancer
+            preds += output
 
-    def get_probability(self) -> str:
-        prob = np.mean(self.predictions) * 100 
-        return "{:.2f}".format(prob)
+        _, global_prediction = max(preds, 1)
+        print(global_prediction)
+        if global_prediction == 0: return "The pacient does not have cancer"
+        elif global_prediction == 1: return "The pacient has cancer"
+        return preds
+
--- public/main.py
+++ public/main.py
@@ -3,8 +3,15 @@ import time
 from BrainTumorDetector import BrainTumorDetector
 
 def start_detection(images):
+    if images == []: 
+        return
+        
     model = BrainTumorDetector()
     response = model.evaluate(images)
+
+    st.info(
+        f"{response}"
+    )
 
 if __name__ == '__main__':
     st.title("Brain Tumor Detection")
